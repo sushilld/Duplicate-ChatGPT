99151e4eff82b7ee2e95e31feae5f894e086f39d
= <sushilldhakal25@gmail.com>
2023-03-27 16:03:30 +0545

    Pushed code to git

---
+++ chat101.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from streamlit_chat import message
+import time
+from chatCode import reply_to_question
+import random
+import keyboard
+
+cnt = 0
+if __name__ == '__main__':
+    st.header('Duplicate ChatGPT')
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+
+    with st.form('form', clear_on_submit=True):
+        cnt += 1
+        user_input = st.text_input('enter message : ', '')
+        submitted = st.form_submit_button('submit', use_container_width=10)
+    
+    style = """
+    <style>
+     div[class='css-1p05t8e epcbefy1']{
+                background: rgba(0, 0, 0, 0.3);
+                position: fixed;
+                bottom: 0;
+                transform: translate(-50%, -50%);
+                width: 40%;
+                left: 50%;
+                z-index: 999;
+                margin-top: 10rem;
+              }
+    </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+
+    if submitted and user_input:
+        
+        ans = reply_to_question(user_input)
+        print(ans)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(ans)
+        if 'key' not in st.session_state:
+            st.session_state['key'] = time.time() + random.randint(0, 100)
+
+        else:
+            st.session_state['key'] = time.time() + random.randint(0, 100)
+
+       
+    for i in range(len(st.session_state['past'])):
+        message(st.session_state['past'][i], is_user=True,key=str(time.time() + random.randint(100, 200)))
+        if len(st.session_state['generated']) > i:
+            message(st.session_state['generated'][i], key= str(time.time() + random.randint(300, 400)))
---
+++ chatCode.py
@@ -0,0 +1,44 @@
+import openai
+# from TTS import speakingText
+
+openai.api_key = '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'
+
+
+def reply_to_question(question, prev_question_list = []):
+  if len(prev_question_list) == 0:
+    few_shot_text = '''### Answer questions like a chat-bot. Also take into account the previous questions and answers.
+                      [Question] Who are you?
+                      [Answer] Hi!! My name is Sushil. How may I help you?
+                      [Question] Thanks for helping me.
+                      [Answer] My pleasure!! If you have any other questions please do ask me.
+                      [Question] Bye
+                      [Answer] Bye Have a nice day.
+                      [Question]'''
+  else:
+    few_shot_text = f'''### Answer questions like a chat-bot. Also take into account the previous questions and answers.
+                      [Question] Who are you?
+                      [Answer] Hi!! My name is Sushil. How ma
+                      [Question] Thanks for helping me.
+                      [Answer] My pleasure!! If you have any other questions please do ask me.
+                      [Question] Bye
+                      [Answer] Bye Have a nice day.
+                      [Previous question] What is the natural satellite of the Earth?
+                      [Question] What is the diameter of it?
+                      [Answer] The diameter of Moon is 3474.2 km.'''
+    for i in range(len(prev_question_list)):
+      few_shot_text += f'''
+                      [Previous Question] {prev_question_list[i]}
+                      [Question]'''
+                      
+  gpt_prompt = f'{few_shot_text} {question} [Answer]'
+  response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=gpt_prompt,
+    temperature=0.7,
+    max_tokens=512,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+  )
+  response_text = str(response['choices'][0]['text'])
+  return response_text
